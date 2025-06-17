01251c8c16d5c3eacfd26175d1a2dd9435fcd179
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-06-17 23:24:51 +0530

    feat: add book existence checks in review controllers and implement pagination for reviews

--- src/api/v1/books/reviews/reviews.controllers.js
+++ src/api/v1/books/reviews/reviews.controllers.js
@@ -3,11 +3,17 @@ import { asyncHandler } from '../../../../utils/async-handler.js';
 import { APIError } from '../../../error.api.js';
 import { APIResponse } from '../../../response.api.js';
 import { Review } from '../../../../models/review.models.js';
+import { Book } from '../../../../models/book.models.js';
+import { paginateResources } from '../../../../utils/paginate.js';
 
 // @controller POST /
 export const addReview = asyncHandler(async (req, res) => {
   // get data from request body
   const { comment } = req.body;
+
+  // check if book exists
+  const existingBook = await Book.findById(req.params.bookId);
+  if (!existingBook) throw new APIError(404, 'Add Review Error', 'Book not found');
 
   // check if a reivew already exists for the book by the user
   const existingReview = await Review.findOne({
@@ -37,19 +43,32 @@ export const addReview = asyncHandler(async (req, res) => {
 
 // @controller GET /
 export const getAllReviews = asyncHandler(async (req, res) => {
+  // check if book exists
+  const existingBook = await Book.findById(req.params.bookId);
+  if (!existingBook) throw new APIError(404, 'Get Reviews Error', 'Book not found');
+
   // get all reviews for a book
-  const allReviews = await Review.find({ bookId: req.params.bookId })
+  const allReviews = await Review.find({ bookId: existingBook._id })
     .populate('userId', '_id username')
     .select('-bookId -__v');
 
+  // paginate reviews
+  const paginatedReviews = paginateResources(allReviews, 10);
+
   // success status to user
-  return res.status(200).json(new APIResponse(200, 'All reviews fetched successfully', allReviews));
+  return res
+    .status(200)
+    .json(new APIResponse(200, 'All reviews fetched successfully', paginatedReviews));
 });
 
 // @controller DELETE /:reviewId
 export const deleteReview = asyncHandler(async (req, res) => {
   // get bookId and reviewId from request params
   const { bookId, reviewId } = req.params;
+
+  // check if book exists
+  const existingBook = await Book.findById(req.params.bookId);
+  if (!existingBook) throw new APIError(404, 'Delete Review Error', 'Book not found');
 
   // check if the review exists for the book by the user
   const existingReview = await Review.findOne({
