ad519f0454fccf74638e6505401117f82f98f2d0
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-06-19 01:04:43 +0530

    feat: add createCart controller with validation and response handling

---
+++ src/api/v1/carts/carts.controllers.js
@@ -0,0 +1,34 @@
+// import local modules
+import { asyncHandler } from '../../../utils/async-handler.js';
+import { APIError } from '../../error.api.js';
+import { APIResponse } from '../../response.api.js';
+import { Cart } from '../../../models/cart.models.js';
+
+// @controller POST /
+export const createCart = asyncHandler(async (req, res) => {
+  // check if cart already exists for the user
+  const existingCart = await Cart.findOne({ userId: req.user.id });
+  if (existingCart)
+    throw new APIError(400, 'Create Cart Error', 'Cart already exists for this user');
+
+  // create a new cart
+  const newCart = await Cart.create({ userId: req.user.id });
+  if (!newCart)
+    throw new APIError(500, 'Create Cart Error', 'Something went wrong while creating the cart');
+
+  // success status to user
+  return res.status(201).json(
+    new APIResponse(201, 'Cart created successfully', {
+      _id: newCart._id,
+      items: newCart.items,
+      totalQuantity: newCart.totalQuantity,
+      totalPrice: newCart.totalPrice,
+    })
+  );
+});
+
+// @controller GET /
+export const getCart = asyncHandler(async (req, res) => {});
+
+// @controller PUT /
+export const updateCart = asyncHandler(async (req, res) => {});
